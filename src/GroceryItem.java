c12dcc104ad3520642e837f3827924fcd84c91f8
ermmorrison <ermorris@lakeheadu.ca>
2021-02-03 11:32:21 -0500

    GroceryItem setName and get methods complete

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -4,4 +4,31 @@ public class GroceryItem {
     private double price;
     private String category;
 
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setName(String name) {
+        if(name.length() > 1)
+            this.name = name;
+        else
+            throw new IllegalArgumentException("Must be a valid name");
+    }
+
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
