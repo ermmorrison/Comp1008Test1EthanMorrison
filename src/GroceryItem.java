e5ad5fa2cb800713cde3da3131c19bc28d9f7e96
ermmorrison <ermorris@lakeheadu.ca>
2021-02-03 11:45:50 -0500

    GroceryItem class Complete

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class GroceryItem {
 
     private String name;
@@ -25,10 +28,23 @@ public class GroceryItem {
 
 
     public void setPrice(double price) {
+        if (price > 0 && price < 150)
         this.price = price;
+        else
+            throw new IllegalArgumentException("Price must be between 0 and 150");
     }
 
     public void setCategory(String category) {
+        category = category.toLowerCase();
+        if (getValidCategories().contains(category));
         this.category = category;
+    }
+
+    public static List<String> getValidCategories() {
+        return Arrays.asList("vegetable","fruit","meat","fish","dairy","bread");
+    }
+
+    public String toString() {
+        return name + " Category: " + category + " Price:" + price + "$";
     }
 }
